81acf19f6a575729bff09b866df455cf09f8dfab
Nii <bnstephens001@gmail.com>
2023-07-14 05:12:53 +0000

    Fixing the issue showing total, edited codes in index and script files

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const button = document.getElementById("calculate");
 const billInput = document.getElementById("billAmount");
 const tipInput = document.getElementById("tipPercentage");
-const totalSpanValue = document.getElementById("total");
+const totalSpanValue = document.getElementById("calculate");
 
 
 function calculateTotal() {
+    preventDefault();
     const billValue = billInput.value;
-    console.log(billValue);
     const tipValue = tipInput.value;
-    console.log(tipValue);
     const totalValue = billValue * (1 + tipValue / 100);
-    totalSpanValue.innerHTML = totalValue.toFixed(2);
-
+    console.log(totalValue);
+    // totalSpanValue.innerText = totalValue.toFixed(2);
+    
 }
+
 
 button.addEventListener("click", calculateTotal)
