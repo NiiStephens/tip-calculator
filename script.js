91cb37ce306f7dfd5aa15a885b2852975e1a3163
Nii <bnstephens001@gmail.com>
2023-07-31 21:43:27 +0000

    Modified index and script files

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-const button = document.getElementById("calculate");
+const btnEl = document.getElementById("calculate");
 const billInput = document.getElementById("billAmount");
 const tipInput = document.getElementById("tipPercentage");
-const totalSpanValue = document.getElementById("calculate");
+const totalSpan = document.getElementById("total");
 
 
 function calculateTotal() {
-    preventDefault();
+    // preventDefault();
     const billValue = billInput.value;
     const tipValue = tipInput.value;
     const totalValue = billValue * (1 + tipValue / 100);
-    console.log(totalValue);
-    // totalSpanValue.innerText = totalValue.toFixed(2);
-    
+    totalSpan.innerText = totalValue.toFixed(2);
 }
 
 
-button.addEventListener("click", calculateTotal)
+btnEl.addEventListener("click", calculateTotal)
